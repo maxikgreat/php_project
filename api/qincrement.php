9f305847bc64009da416f6474b2098891e1d5084
imakarevich <48916169+imakarevich@users.noreply.github.com>
2020-06-21 01:03:57 +0200

    qincr

--- api/qincrement.php
+++ api/qincrement.php
@@ -4,8 +4,8 @@ $_POST = json_decode(file_get_contents("php://input"), true);
 
 
 
-
-$sql = "INSERT INTO `quiz` `(id, name, description, date, question_amount)` VALUES ('$_POST['id']', '$_POST['name']', '$_POST['description']', $_POST['date']', '$_POST['question_amount']');";
+//Create quiz table w/o questions amount
+$sql = "INSERT INTO `quiz` `(name, description, date)` VALUES ('$_POST['name']', '$_POST['description']', '$_POST['date']');";
 
 if($result = mysqli_query($conn, $sql))
 {
@@ -16,8 +16,32 @@ else
     echo "Fatal error!";
 }
 
+//Take uploaded quiz id
+$sql = "SELECT id FROM quiz ORDER BY id DESC LIMIT 1";
+$result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_assoc($result);
+$quiz_id = $row[id];
 
-$sql = "INSERT INTO `questions` `(id, question, quiz_id, correct_answer_id, answer_1, answer_2, answer_3, answer_4)` VALUES (id, question, quiz_id, correct_answer_id, answer_1, answer_2, answer_3, answer_4);";
+//Create questions table
+$sql = "INSERT INTO `questions` `(question, quiz_id, correct_answer_id, answer_1, answer_2, answer_3, answer_4)` VALUES ('$_POST['question']', '$quiz_id', '$_POST['rightAnswer']', '$_POST['answer1']', '$_POST['answer2']', '$_POST['answer3']', '$_POST['answer4)']';";
+
+if($result = mysqli_query($conn, $sql))
+{
+    echo "Success!";
+}
+else
+{
+    echo "Fatal error!";
+}
+
+//Take questions amount
+$sql = "SELECT COUNT(*) FROM questions WHERE id = '$quiz_id'";
+$result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_assoc($result);
+$questions_amount = $row[COUNT(*)];
+
+//Upload questions_amount info into Quiz table
+$sql = "UPDATE quiz SET question_amount = '$questions_amount' WHERE id = '$quiz_id';"
 
 if($result = mysqli_query($conn, $sql))
 {
